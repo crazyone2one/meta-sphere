0ad46705e6c671457ab716b53bf60b00776e6093
lijinglin <the2ndindec@gmail.com>
2025-10-10 17:21:37 +0800

    init repo

---
+++ backend/src/main/java/com/master/meta/MetaSphereApplication.java
@@ -0,0 +1,13 @@
+package com.master.meta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaSphereApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/master/meta/MetaSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.meta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
