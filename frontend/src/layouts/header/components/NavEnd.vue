e5993a6ffd7bd21a8972eab16883ab9d80585da5
lijinglin <the2ndindec@gmail.com>
2025-10-16 16:43:26 +0800

    feat(layout): 添加头部导航用户菜单组件
    - 新增 NavEnd.vue 组件实现用户头像下拉菜单
    - 集成用户资料与退出登录功能
    - 实现退出登录确认弹窗
    - 在头部布局中引入并使用新组件

---
+++ frontend/src/layouts/header/components/NavEnd.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import {computed} from "vue";
+import {useUserStore} from "/@/store";
+import router from "/@/router";
+import {authApi} from "/@/api/modules/auth";
+
+const userStore = useUserStore()
+const options = computed(() => {
+  return [
+    {
+      label: '用户资料',
+      key: 'profile',
+    },
+    {
+      label: '退出登录',
+      key: 'logout',
+    }
+  ]
+})
+const userInitial = computed(() => {
+  if (userStore.name) {
+    // 如果用户名存在，返回首字母
+    return userStore.name.charAt(0).toUpperCase();
+  } else if (userStore.email) {
+    // 如果邮箱存在，返回邮箱首字母
+    return userStore.email.charAt(0).toUpperCase();
+  } else {
+    // 默认显示
+    return 'M';
+  }
+})
+const logout = () => {
+  window.$dialog.warning({
+    title: '确定要退出登录吗？',
+    positiveText: '确定',
+    negativeText: '取消',
+    onPositiveClick: () => {
+      authApi.logout().then(() => {
+        const currentRoute = router.currentRoute.value;
+        router.push({
+          name: 'login',
+          query: {
+            ...router.currentRoute.value.query,
+            redirect: currentRoute.name as string,
+          }
+        })
+        userStore.resetInfo()
+      })
+    },
+  })
+}
+const handleSelect = (key: string) => {
+  switch (key) {
+    case 'profile':
+      break;
+    case 'logout':
+      logout()
+      break;
+  }
+}
+</script>
+
+<template>
+  <n-dropdown trigger="click" :options="options" @select="handleSelect">
+    <n-avatar round :size="26" class="cursor-pointer ml-[5px] color-purple">
+      {{ userInitial }}
+    </n-avatar>
+  </n-dropdown>
+</template>
+
+<style scoped>
+
+</style>
