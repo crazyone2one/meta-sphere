17b5d5440b8b1b7f8e924be8f6ff59d9518c9c99
lijinglin <the2ndindec@gmail.com>
2025-10-16 17:02:09 +0800

    feat(utils): 添加随机工具类支持随机行选择和双精度字符串生成
    - 新增 RandomUtil 工具类- 实现从列表中随机选取指定数量元素的方法
    - 实现生成指定范围内的随机双精度数值字符串的方法
    - 添加类和方法的基本文档注释
    - 集成到现有工具包结构中

---
+++ backend/src/main/java/com/master/meta/utils/RandomUtil.java
@@ -0,0 +1,23 @@
+package com.master.meta.utils;
+
+import com.mybatisflex.core.row.Row;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @author Created by 11's papa on 2025/10/15
+ */
+public class RandomUtil {
+    public static List<Row> getRandomSubList(List<Row> originalList, int count) {
+        List<Row> result = new ArrayList<>(originalList);
+        Collections.shuffle(result);
+        // 返回前count个元素
+        return result.subList(0, Math.min(count, result.size()));
+    }
+    public static String doubleTypeString(int min, int max) {
+        return String.format("%.2f%n", min + ((max - min) * new Random().nextDouble()));
+    }
+}
