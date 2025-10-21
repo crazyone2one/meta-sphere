3ce4a4f56d84fb40b396bce88e038ef2cc967c95
lijinglin <the2ndindec@gmail.com>
2025-10-21 10:30:40 +0800

    feat(schedule): 更新自定义配置功能以支持更多传感器选项
    - 修改 CustomConfig 类中的 sensorIds 字段类型为 String
    - 添加 sensorType 和 sensorValueType 字段到 CustomConfig 类
    - 实现 isNotEmpty 方法用于判断 CustomConfig 对象是否包含有效数据- 在前端 CustomConfigModal 组件中移除预设的传感器选项并动态获取
    - 根据选中的传感器选项设置 sensorType 和 sensorValueType 值- 调整 dynamic-form-drawer 中字段禁用按钮的位置并在相关方法中增加调试日志

--- backend/src/main/java/com/master/meta/dto/CustomConfig.java
+++ backend/src/main/java/com/master/meta/dto/CustomConfig.java
@@ -10,11 +10,25 @@ import java.util.List;
 @Data
 public class CustomConfig {
     // 测点sensor_id
-    private List<String> sensorIds;
+    private String sensorIds;
     // 阈值区间
     private List<Double> thresholdInterval;
-    // 超阈值
+    // 是否超过阈值
     private Boolean superthreshold;
     // 报警标志
     private Boolean alarmFlag;
+    private String sensorType;
+    private String sensorValueType;
+
+    /**
+     * 判断CustomConfig是否不为空
+     *
+     * @return 如果对象不为null且至少有一个字段有值则返回true，否则返回false
+     */
+    public boolean isNotEmpty() {
+        return (sensorIds != null && !sensorIds.isEmpty()) ||
+                (thresholdInterval != null && !thresholdInterval.isEmpty()) ||
+                superthreshold != null ||
+                alarmFlag != null || sensorType != null || sensorValueType != null;
+    }
 }
