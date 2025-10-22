dbcc369d130a05e3f4964ebc55563e7a02b9f48e
lijinglin <the2ndindec@gmail.com>
2025-10-22 09:39:59 +0800

    feat(utils): 添加随机字符串生成工具方法- 新增 generateRandomString 方法用于生成指定长度的随机字母数字字符串 - 添加 SensorMNType 类型支持，扩展 generateRandomDoubleString 方法 - 引入 ALPHA_NUMERIC_STRING 常量定义可用字符集-为随机数生成逻辑增加更灵活的重载方法

--- backend/src/main/java/com/master/meta/utils/RandomUtil.java
+++ backend/src/main/java/com/master/meta/utils/RandomUtil.java
@@ -1,5 +1,6 @@
 package com.master.meta.utils;
 
+import com.master.meta.constants.SensorMNType;
 import com.mybatisflex.core.row.Row;
 
 import java.util.ArrayList;
@@ -11,16 +12,42 @@ import java.util.Random;
  * @author Created by 11's papa on 2025/10/15
  */
 public class RandomUtil {
+    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String generateRandomString(int length) {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
+            builder.append(ALPHA_NUMERIC_STRING.charAt(index));
+        }
+
+        return builder.toString();
+    }
+
     public static List<Row> getRandomSubList(List<Row> originalList, int count) {
         List<Row> result = new ArrayList<>(originalList);
         Collections.shuffle(result);
         // 返回前count个元素
         return result.subList(0, Math.min(count, result.size()));
     }
+
     public static String doubleTypeString(int min, int max) {
         return String.format("%.2f%n", min + ((max - min) * new Random().nextDouble()));
     }
+
     public static String generateRandomDoubleString(double min, double max) {
         return String.format("%.2f", min + ((max - min) * new Random().nextDouble()));
+    }
+
+    /**
+     * 生成指定传感器类型范围内的随机双精度浮点数字符串
+     *
+     * @param sensorMNType 传感器类型，用于确定随机数的最小值和最大值范围
+     * @return 在传感器类型最小值和最大值之间的随机双精度浮点数字符串，保留两位小数
+     */
+    public static String generateRandomDoubleString(SensorMNType sensorMNType) {
+        return String.format("%.2f", sensorMNType.getMinValue() + ((sensorMNType.getMaxValue() - sensorMNType.getMinValue()) * new Random().nextDouble()));
     }
 }
