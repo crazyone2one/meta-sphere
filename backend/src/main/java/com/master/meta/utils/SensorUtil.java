0874f8900bde34c469f102cb6b1ec1804fedcaac
lijinglin <the2ndindec@gmail.com>
2025-10-23 15:52:41 +0800

    refactor(utils):重构传感器工具类以支持多种传感器类型
    - 移除不再使用的 SensorType 类导入
    - 移除未使用的 activeProfile 字段及相关逻辑- 新增对 SensorKGType 和 SensorMNType 类型的支持
    - 优化从 Redis 获取传感器数据的逻辑- 统一 CDSS传感器列表获取方法签名
    - 移除开发环境下的文件生成日志输出

--- backend/src/main/java/com/master/meta/utils/SensorUtil.java
+++ backend/src/main/java/com/master/meta/utils/SensorUtil.java
@@ -1,11 +1,11 @@
 package com.master.meta.utils;
 
-import com.master.meta.constants.SensorType;
+import com.master.meta.constants.SensorKGType;
+import com.master.meta.constants.SensorMNType;
 import com.mybatisflex.core.datasource.DataSourceKey;
 import com.mybatisflex.core.row.Db;
 import com.mybatisflex.core.row.Row;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -20,8 +20,6 @@ import java.util.Map;
 @Slf4j
 @Component
 public class SensorUtil {
-    @Value("${spring.profiles.active:default}")
-    private String activeProfile;
     private final RedisService redisService;
 
     public SensorUtil(RedisService redisService) {
@@ -54,21 +52,43 @@ public class SensorUtil {
         }
     }
 
+    public List<Row> getSensorFromRedis(String projectNum, SensorMNType sensorMNType, Boolean deleted) {
+        String rainDefineInRedis = redisService.getSensor(projectNum, sensorMNType.getKey());
+        if (rainDefineInRedis != null) {
+            return JSON.parseArray(rainDefineInRedis, Row.class);
+        } else {
+            List<Row> sensorList = getRainDefineList(sensorMNType.getTableName(), deleted);
+            redisService.storeSensor(projectNum, sensorMNType.getKey(), sensorList, 60 * 60 * 24 * 7);
+            return sensorList;
+        }
+    }
+
     /**
      * 获取CDSS传感器列表
      *
-     * @param projectNum 项目编号
-     * @param sensorType 传感器类型
-     * @param deleted    是否删除
+     * @param projectNum   项目编号
+     * @param sensorMNType 传感器类型
+     * @param deleted      是否删除
      * @return 传感器列表
      */
-    public List<Row> getCDSSSensorFromRedis(String projectNum, SensorType sensorType, Boolean deleted) {
-        String sensorListInRedis = redisService.getSensor(projectNum, sensorType.getKey());
+    public List<Row> getCDSSSensorFromRedis(String projectNum, SensorMNType sensorMNType, Boolean deleted) {
+        String sensorListInRedis = redisService.getSensor(projectNum, sensorMNType.getKey());
         if (sensorListInRedis != null) {
             return JSON.parseArray(sensorListInRedis, Row.class);
         } else {
-            List<Row> sensorList = getCDSSList(sensorType.getTableName(), deleted);
-            redisService.storeSensor(projectNum, sensorType.getKey(), sensorList, 60 * 60 * 24 * 7);
+            List<Row> sensorList = getCDSSList(sensorMNType.getTableName(), deleted);
+            redisService.storeSensor(projectNum, sensorMNType.getKey(), sensorList, 60 * 60 * 24 * 7);
+            return sensorList;
+        }
+    }
+
+    public List<Row> getCDSSSensorFromRedis(String projectNum, SensorKGType sensorKGType, Boolean deleted) {
+        String sensorListInRedis = redisService.getSensor(projectNum, sensorKGType.getKey());
+        if (sensorListInRedis != null) {
+            return JSON.parseArray(sensorListInRedis, Row.class);
+        } else {
+            List<Row> sensorList = getCDSSList(sensorKGType.getTableName(), deleted);
+            redisService.storeSensor(projectNum, sensorKGType.getKey(), sensorList, 60 * 60 * 24 * 7);
             return sensorList;
         }
     }
@@ -89,10 +109,6 @@ public class SensorUtil {
     }
 
     public void generateFile(String filePath, String content, String type) {
-        if (activeProfile.equals("dev")) {
-            log.info("{}", content);
-            return;
-        }
         FileWriter fw = null;
         try {
             File file = new File(filePath);
