8e664b4d69f6cadb7c7c4cda0bb9c9e9aba37874
lijinglin <the2ndindec@gmail.com>
2025-11-14 08:43:29 +0800

    refactor(utils):优化随机数生成器实例化
    - 将Random实例改为静态常量，避免重复创建
    - 统一使用静态random实例生成随机数

--- backend/src/main/java/com/master/meta/utils/RandomUtil.java
+++ backend/src/main/java/com/master/meta/utils/RandomUtil.java
@@ -13,11 +13,10 @@ import java.util.Random;
  */
 public class RandomUtil {
     private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final Random random = new Random();
 
     public static String generateRandomString(int length) {
-        Random random = new Random();
         StringBuilder builder = new StringBuilder(length);
-
         for (int i = 0; i < length; i++) {
             int index = random.nextInt(ALPHA_NUMERIC_STRING.length());
             builder.append(ALPHA_NUMERIC_STRING.charAt(index));
@@ -34,11 +33,11 @@ public class RandomUtil {
     }
 
     public static String doubleTypeString(int min, int max) {
-        return String.format("%.2f%n", min + ((max - min) * new Random().nextDouble()));
+        return String.format("%.2f%n", min + ((max - min) * random.nextDouble()));
     }
 
     public static String generateRandomDoubleString(double min, double max) {
-        return String.format("%.2f", min + ((max - min) * new Random().nextDouble()));
+        return String.format("%.2f", min + ((max - min) * random.nextDouble()));
     }
 
     /**
